6ea40970eac766271aea5cbea457a015d1e644b4
Isara Nakavisute <isara.n@pch.com>
2025-06-29 14:26:23 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+ <div class="flex flex-row items-center justify-center w-full h-screen bg-yellow-300">
+
+  <div class="flex flex-row items-center justify-center w-1/2 h-screen bg-yellow-300">
+   <div class="flex flex-row items-center justify-center w-4/5 bg-green-200 h-4/5">
+    <div class="flex flex-row items-center justify-center w-4/5 bg-black h-4/5">
+     <div class="items-center justify-center text-4xl text-white">
+      Test
+     </div>
+    </div>
+   </div>
+  </div>
+
+
+  <div class="flex flex-row items-center justify-center w-1/2 h-screen bg-yellow-300">
+   <div class="flex flex-row items-center justify-center w-4/5 bg-green-200 h-4/5">
+    <div class="flex flex-row items-center justify-center w-4/5 bg-black h-4/5">
+     <div class="items-center justify-center text-4xl text-white">
+      Test
+     </div>
+    </div>
+   </div>
+  </div>
+
+</div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: 'App',
+};
+</script>
